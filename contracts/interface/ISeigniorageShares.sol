d19df709d9450e631592d5880c3f5f9f9544b99e
Robert <69215840+FITvbVlQXPo01TLNLo7O00hv5Lp4KRLBB9WO0eP@users.noreply.github.com>
2021-01-04 14:30:38 -0600

    Update and rename interface/ISeigniorageShares.sol to contracts/interface/ISeigniorageShares.sol

--- contracts/interface/ISeigniorageShares.sol
+++ contracts/interface/ISeigniorageShares.sol
@@ -5,13 +5,12 @@ interface ISeigniorageShares {
     function setDividendPoints(address account, uint256 totalDividends) external returns (bool);
     function setDividendPointsEuro(address account, uint256 totalDividends) external returns (bool);
 
-    function mintShares(address account, uint256 amount) external returns (bool);
-
     function lastDividendPoints(address who) external view returns (uint256);
     function lastDividendPointsEuro(address who) external view returns (uint256);
+    function stakingStatus(address who) external view returns (uint256);
     
     function externalRawBalanceOf(address who) external view returns (uint256);
     function externalTotalSupply() external view returns (uint256);
 
-    function deleteShare(address account, uint256 amount) external;
+    function totalStaked() external view returns (uint256);
 }
